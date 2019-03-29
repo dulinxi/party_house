5e312bda42fb07b604e29488b13d6efc2a5df8b1
ffey <95609658@qq.com>
2019-03-29 15:47:26 +0800

    add financial page

--- lib/src/routes/financial/index.dart
+++ lib/src/routes/financial/index.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 // import 'package:party_house/src/widget/icon.dart';
-import 'bank_card.dart';
-import 'botttom_nav_bar.dart';
 
-import 'package:party_house/src/routes/financial/index.dart';
-
-class HomePage extends StatefulWidget {
+class FinancialPage extends StatefulWidget {
   @override
-  createState() => _HomePageState();
+  createState() => _FinancialPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _FinancialPageState extends State<FinancialPage> {
+  final GlobalKey<AnimatedCircularChartState> _chartKey =
+      new GlobalKey<AnimatedCircularChartState>();
   @override
   Widget build(BuildContext context) {
     final _stateBarHeight = MediaQuery.of(context).padding.top;
@@ -28,35 +27,8 @@ class _HomePageState extends State<HomePage> {
                   height: _stateBarHeight,
                 ),
                 ListTile(
-                  leading: GestureDetector(
-                    onTap: (){
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return FinancialPage();
-                        }
-                      ));
-                    },
-                    child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 2.0),
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                    child: ClipOval(
-                      child: Image.network(
-                        'http://b-ssl.duitang.com/uploads/item/201811/20/20181120204138_irzdb.jpg',
-                        width: 40,
-                        height: 40,
-                      ),
-                    ),
-                  ),
-                  ),
-                  title: Text(
-                    'James Holland',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  subtitle: Text(
-                    'james2@.com',
-                    style: TextStyle(color: Colors.white),
+                  leading: BackButton(
+                    color: Colors.white,
                   ),
                   trailing: Icon(
                     Icons.notifications,
@@ -64,42 +36,44 @@ class _HomePageState extends State<HomePage> {
                     size: 20.0,
                   ),
                 ),
-                SizedBox(
-                  height: 10.0,
-                ),
                 Padding(
                   padding: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        width: 4.0,
-                        height: 4.0,
-                        margin: EdgeInsets.only(right: 5.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(3.0),
+                  child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text('Your Wallet', style: TextStyle(color: Colors.white),),
+                      ],
+                    ),
+                    SizedBox(height: 10.0,),
+                    Row(
+                      children: <Widget>[
+                        Text('\$1,750.50', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18.0),),
+                        SizedBox(width: 10.0,),
+                        Container(
+                          padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15.0),
+                            color: Color.fromRGBO(255, 84, 132, 1),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.keyboard_arrow_up, color: Colors.white,),
+                              Text('15%', style: TextStyle(color: Colors.white),),
+                            ],
+                          ),
                         ),
-                      ),
-                      Container(
-                        width: 4.0,
-                        height: 4.0,
-                        margin: EdgeInsets.only(right: 5.0),
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(3.0),
-                        ),
-                      ),
-                      Container(
-                        width: 4.0,
-                        height: 4.0,
-                        margin: EdgeInsets.only(right: 5.0),
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(3.0),
-                        ),
-                      ),
-                    ],
-                  ),
+                      ],
+                    ),
+                  ],
+                ),
+                ),
+                SizedBox(
+                  height: 10.0,
                 ),
                 Container(
                   height: 190,
@@ -120,23 +94,75 @@ class _HomePageState extends State<HomePage> {
                         left: 0,
                         right: 0,
                         child: Container(
+                          padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
                           height: 180,
-                          child: SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Padding(
-                                padding: EdgeInsets.all(10.0),
-                                child: Row(
-                                  children: <Widget>[
-                                    BankCard(),
-                                    BankCard(
-                                      carcColor: Colors.orange,
+                          child: Card(
+                              child: Container(
+                            padding: EdgeInsets.all(20.0),
+                            child: Column(
+                              children: <Widget>[
+                                ListTile(
+                                  leading: Container(
+                                    width: 45,
+                                    height: 45,
+                                    alignment: Alignment.center,
+                                    child: AnimatedCircularChart(
+                                      key: _chartKey,
+                                      size: Size(45.0, 45.0),
+                                      initialChartData: <CircularStackEntry>[
+                                        CircularStackEntry(
+                                          <CircularSegmentEntry>[
+                                            CircularSegmentEntry(
+                                              80.0,
+                                              Colors.green[400],
+                                              rankKey: 'completed',
+                                            ),
+                                            CircularSegmentEntry(
+                                              20.0,
+                                              Colors.white,
+                                              rankKey: 'remaining',
+                                            ),
+                                          ],
+                                          rankKey: 'progress',
+                                        ),
+                                      ],
+                                      chartType: CircularChartType.Radial,
+                                      percentageValues: true,
+                                      holeLabel: '80',
+                                      labelStyle: new TextStyle(
+                                        color: Colors.green[600],
+                                        fontSize: 16.0,
+                                      ),
                                     ),
-                                    BankCard(
-                                        carcColor:
-                                            Color.fromRGBO(255, 84, 122, 1)),
+                                  ),
+                                  title: Text('Good Financial'),
+                                  subtitle: Text(
+                                    'Your financial condition good',
+                                    style: TextStyle(fontSize: 12.0),
+                                  ),
+                                ),
+                                Divider(),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Text(
+                                      'View Statistic',
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromRGBO(132, 24, 194, 1)),
+                                    ),
+                                    Icon(
+                                      Icons.keyboard_arrow_right,
+                                      color: Colors.black38,
+                                    ),
                                   ],
                                 ),
-                              )),
+                              ],
+                            ),
+                          )),
                         ),
                       ),
                     ],
@@ -150,7 +176,7 @@ class _HomePageState extends State<HomePage> {
             child: Row(
               children: <Widget>[
                 Text(
-                  'Recent Transactions',
+                  'Detail Infomation',
                   style: TextStyle(
                     fontSize: 18.0,
                   ),
@@ -166,10 +192,9 @@ class _HomePageState extends State<HomePage> {
                   top: 0,
                   left: 0,
                   right: 0,
-                  bottom: 50,
+                  bottom: 0,
                   child: SingleChildScrollView(
                     child: Container(
-                      padding: EdgeInsets.only(bottom: 80.0),
                       child: Column(
                         children: <Widget>[
                           ListTile(
@@ -226,14 +251,6 @@ class _HomePageState extends State<HomePage> {
                         ],
                       ),
                     ),
-                  ),
-                ),
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: Container(
-                    child: MyBottomBar()
                   ),
                 ),
               ],
